1086b0860e809df248174069ea363ba38bd294dd
kanalumaddela <14265751+kanalumaddela@users.noreply.github.com>
2018-01-30 09:04:56 -0500

    fix passing request

--- src/SteamLoginServiceProvider.php
+++ src/SteamLoginServiceProvider.php
@@ -19,19 +19,20 @@ class SteamLoginServiceProvider extends ServiceProvider
 	 *
 	 * @return void
 	 */
-	public function boot() {
+	public function boot()
+	{
 		$this->publishes([__DIR__ . '/../config/steam-login.php' => config_path('steam-login.php')]);
 	}
 
 	/**
 	 * Register bindings in the container
 	 *
-	 * @param Request $request
 	 * @return void
 	 */
-	public function register(Request $request) {
-		$this->app->singleton('steamlogin', function() use ($request) {
-			return new SteamLogin($request);
+	public function register()
+	{
+		$this->app->singleton('steamlogin', function() {
+			return new SteamLogin($this->app->request);
 		});
 	}
 }
