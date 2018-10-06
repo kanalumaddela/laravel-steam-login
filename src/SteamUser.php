1ad83b4b7b22302b8bc44a538d4b5e2801476ba7
kanalumaddela <github@maddela.org>
2018-10-06 00:51:27 -0400

    - dont extend xpaw's steamid, generic function naming conflicts - use fluent - use magic methods for retrieving/calling stuff

--- src/SteamUser.php
+++ src/SteamUser.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Fluent;
 use SteamID;
 
-class SteamUser extends SteamID
+class SteamUser
 {
     /**
      * Steam Community URL using 64bit steamid.
@@ -31,119 +31,18 @@ class SteamUser extends SteamID
     const STEAM_PLAYER_API = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s';
 
     /**
-     * Fluent instance of player data.
+     * Attributes of a user. e.g. steamid, profile, etc.
+     *
+     * @var \stdClass
+     */
+    public $attributes;
+
+    /**
+     * Fluent instance of user data.
      *
      * @var \Illuminate\Support\Fluent
      */
     public $fluent;
-
-    /**
-     * SteamID - 765611XXXXXXXXXXX.
-     *
-     * @var int
-     */
-    public $steamid;
-
-    /**
-     * SteamID2 - STEAM_X:Y:Z.
-     *
-     * @var string
-     */
-    public $steamid2;
-
-    /**
-     * SteamID3 - [U:1:W].
-     * W = Z*2+Y.
-     *
-     * @var string
-     */
-    public $steamid3;
-
-    /**
-     * Steam AccountID - W.
-     *
-     * @var int
-     */
-    public $accountId;
-
-    /**
-     * Player's small avatar 32x32.
-     *
-     * @var string
-     */
-    public $avatarSmall;
-
-    /**
-     * Player's medium avatar 64x64.
-     *
-     * @var string
-     */
-    public $avatarMedium;
-
-    /**
-     * Player's medium avatar 184x184.
-     *
-     * @var string
-     */
-    public $avatarLarge;
-
-    /**
-     * Player's display name.
-     *
-     * @var string
-     */
-    public $name;
-
-    /**
-     * Player's realname set.
-     *
-     * @var string|null
-     */
-    public $realName;
-
-    /**
-     * Player's online state.
-     *
-     * @var string
-     */
-    public $playerState;
-
-    /**
-     * Player's status message.
-     *
-     * @var string
-     */
-    public $stateMessage;
-
-    /**
-     * Player's privacy setting.
-     *
-     * @var string
-     */
-    public $privacyState;
-
-    /**
-     * Player's privacy setting.
-     *
-     * @var int
-     */
-    public $visibilityState;
-
-    /**
-     * Player's profile URL.
-     * Can be either:
-     *   - https://steamcommunity.com/profiles/<steamid>
-     *   - https://steamcommunity.com/profiles/[U:1:W]
-     *   - https://steamcommunity.com/id/<name>.
-     */
-    public $profileURL;
-
-    /**
-     * Epoch timestamp of joining steam.
-     *
-     * @var int|null
-     */
-    public $joined;
 
     /**
      * Profile data retrieval method to use.
@@ -153,11 +52,18 @@ class SteamUser extends SteamID
     protected $method = 'xml';
 
     /**
-     * URL to use when retrieving a player's profile.
+     * URL to use when retrieving a user's profile.
      *
      * @var string
      */
     protected $profileDataUrl;
+
+    /**
+     * Guzzle instance.
+     *
+     * @var \SteamID
+     */
+    protected $steamId;
 
     /**
      * Guzzle instance.
@@ -187,25 +93,78 @@ class SteamUser extends SteamID
      */
     public function __construct($steamid, GuzzleClient $guzzle = null)
     {
-        parent::__construct($steamid);
-
+        $this->steamId = new SteamID($steamid);
         $this->guzzle = $guzzle ?? new GuzzleClient();
 
-        $this->steamid = $this->ConvertToUInt64();
-        $this->steamid2 = $this->RenderSteam2();
-        $this->steamid3 = $this->RenderSteam3();
-        $this->accountId = $this->GetAccountID();
+        $this->attributes = new \stdClass();
+
+        $this->attributes->steamid = $this->steamId->ConvertToUInt64();
+        $this->attributes->steamid2 = $this->steamId->RenderSteam2();
+        $this->attributes->steamid3 = $this->steamId->RenderSteam3();
+        $this->attributes->accountId = $this->steamId->GetAccountID();
+        $this->attributes->profileUrl = sprintf(self::STEAM_PROFILE, $this->attributes->steamid3);
+        $this->attributes->profileDataUrl = sprintf(self::STEAM_PROFILE.'/?xml=1', $this->attributes->steamid);
+
+        $this->fluent = new Fluent($this->attributes);
 
         $this->method = Config::get('steam-login.method', 'xml') == 'api' ? 'api' : 'xml';
-        $this->profileURL = sprintf(self::STEAM_PROFILE, $this->steamid3);
-        $this->profileDataUrl = $this->method == 'xml' ? sprintf(self::STEAM_PROFILE.'/?xml=1', $this->steamid) : sprintf(self::STEAM_PLAYER_API, Config::get('steam-login.api_key'), $this->steamid);
+        $this->profileDataUrl = $this->method == 'xml' ? $this->attributes->profileDataUrl : sprintf(self::STEAM_PLAYER_API, Config::get('steam-login.api_key'), $this->attributes->steamid);
+
     }
 
+    /**
+     * magic methiod __call
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
+    {
+        if (method_exists($this->fluent, $name)) {
+            return call_user_func_array([$this->fluent, $name], $arguments);
+        }
+        if (method_exists($this->steamId, $name)) {
+            return call_user_func_array([$this->steamId, $name], $arguments);
+        }
+        if (substr($name, 0, 3) === "get") {
+            $property = lcfirst(substr($arguments[0], 3));
+
+            return call_user_func_array([$this, '__get'], [$property]);
+        }
+
+        throw new \Exception('Unknown method '.$name);
+    }
+
+    /**
+     * magic method __get
+     *
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->fluent->__get($name);
+    }
+
+    /**
+     * magic method __toString using Fluent toJson()
+     *
+     * @return string
+     */
     public function __toString()
     {
-        return 'todo';
+        return $this->fluent->toJson();
     }
 
+    /**
+     * Retrieve a user's steam info set its attributes
+     *
+     * @return $this
+     */
     public function getUserInfo()
     {
         $this->userInfo();
@@ -214,7 +173,7 @@ class SteamUser extends SteamID
     }
 
     /**
-     * Retrieve a player's profile info from Steam via API or XML data.
+     * Retrieve a user's profile info from Steam via API or XML data.
      */
     private function userInfo()
     {
@@ -226,34 +185,36 @@ class SteamUser extends SteamID
                 $data = isset($data->response->players[0]) ? $data->response->players[0] : null;
 
                 if ($data) {
-                    $this->name = $data->personaname;
-                    $this->realName = !empty($data->realname) ? $data->realname : null;
-                    $this->playerState = $data->personastate != 0 ? 'Online' : 'Offline';
-                    $this->privacyState = ($data->communityvisibilitystate == 1 || $data->communityvisibilitystate == 2) ? 'Private' : 'Public';
-                    $this->stateMessage = isset(self::$personaStates[$data->personastate]) ? self::$personaStates[$data->personastate] : $data->personastate;
-                    $this->visibilityState = $data->communityvisibilitystate;
-                    $this->avatarSmall = $data->avatar;
-                    $this->avatarMedium = $data->avatarmedium;
-                    $this->avatarLarge = $data->avatarfull;
-                    $this->joined = isset($data->timecreated) ? $data->timecreated : null;
+                    $this->attributes->name = $data->personaname;
+                    $this->attributes->realName = !empty($data->realname) ? $data->realname : null;
+                    $this->attributes->playerState = $data->personastate != 0 ? 'Online' : 'Offline';
+                    $this->attributes->privacyState = ($data->communityvisibilitystate == 1 || $data->communityvisibilitystate == 2) ? 'Private' : 'Public';
+                    $this->attributes->stateMessage = isset(self::$personaStates[$data->personastate]) ? self::$personaStates[$data->personastate] : $data->personastate;
+                    $this->attributes->visibilityState = $data->communityvisibilitystate;
+                    $this->attributes->avatarSmall = $data->avatar;
+                    $this->attributes->avatarMedium = $data->avatarmedium;
+                    $this->attributes->avatarLarge = $data->avatarfull;
+                    $this->attributes->joined = isset($data->timecreated) ? $data->timecreated : null;
                 }
                 break;
             case 'xml':
                 if ($data !== false && !isset($data->error)) {
-                    $this->name = (string) $data->steamID;
-                    $this->realName = !empty($data->realName) ? $data->realName : null;
-                    $this->playerState = ucfirst($data->onlineState);
-                    $this->privacyState = ($data->privacyState == 'friendsonly' || $data->privacyState == 'private') ? 'Private' : 'Public';
-                    $this->stateMessage = (string) $data->stateMessage == 'Last Online ' ? 'Last Online: Unknown' : $data->stateMessage;
-                    $this->visibilityState = (int) $data->visibilityState;
-                    $this->avatarSmall = (string) $data->avatarIcon;
-                    $this->avatarMedium = (string) $data->avatarMedium;
-                    $this->avatarLarge = (string) $data->avatarFull;
-                    $this->joined = isset($data->memberSince) ? strtotime($data->memberSince) : null;
+                    $this->attributes->name = (string) $data->steamID;
+                    $this->attributes->realName = !empty($data->realName) ? $data->realName : null;
+                    $this->attributes->playerState = ucfirst($data->onlineState);
+                    $this->attributes->privacyState = ($data->privacyState == 'friendsonly' || $data->privacyState == 'private') ? 'Private' : 'Public';
+                    $this->attributes->stateMessage = (string) $data->stateMessage == 'Last Online ' ? 'Last Online: Unknown' : $data->stateMessage;
+                    $this->attributes->visibilityState = (int) $data->visibilityState;
+                    $this->attributes->avatarSmall = (string) $data->avatarIcon;
+                    $this->attributes->avatarMedium = (string) $data->avatarMedium;
+                    $this->attributes->avatarLarge = (string) $data->avatarFull;
+                    $this->attributes->joined = isset($data->memberSince) ? strtotime($data->memberSince) : null;
                 }
                 break;
             default:
                 break;
         }
+
+        $this->fluent = new Fluent($this->attributes);
     }
 }
