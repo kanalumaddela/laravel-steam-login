578da99f1d157873707360225f434bfbe5b93458
Sam <github@maddela.org>
2018-10-13 02:37:00 -0400

    Update SteamLogin.php

--- src/SteamLogin.php
+++ src/SteamLogin.php
@@ -269,7 +269,7 @@ class SteamLogin implements SteamLoginInterface
 
         preg_match('#^https?://steamcommunity.com/openid/id/([0-9]{17,25})#', $this->request->input('openid_claimed_id'), $matches);
         $steamid = is_numeric($matches[1]) ? $matches[1] : 0;
-        $steamid = preg_match("#is_valid\s*:\s*true#i", $result) === 1 ? $steamid : null;
+        $steamid = preg_match("#is_valid\s*:\s*true#i", $result) === 1 ? (int) $steamid : null;
 
         return $steamid;
     }
